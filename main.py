eb02498d626a4c7f36336f393b2621dc03eb6d18
Dan Y <liquidcore7@gmail.com>
2019-01-15 17:00:30 +0200

    fix endpoints and procfile

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ from controllers.session import SessionController
 app = Flask(__name__)
 global_api = Api(app)
 
-global_api.add_resource(SessionController, '/session')  # GET
-global_api.add_resource(SessionController, '/session/<session_id: int>')  # POST
+global_api.add_resource(SessionController, '/session', endpoint='/session')  # GET
+global_api.add_resource(SessionController, '/session/<session_id>', endpoint='/session/<session_id>')  # POST
 
-global_api.add_resource(DummyController, '/test')  # GET
-global_api.add_resource(DummyController, '/test/<data: str>')  # POST
+global_api.add_resource(DummyController, '/test', endpoint='/test')  # GET
+global_api.add_resource(DummyController, '/test/<data>', endpoint='/test/<data>')  # POST
 
 if __name__ == '__main__':
     app.run()
