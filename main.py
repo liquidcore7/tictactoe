2efb908f8624127b9686e720d3078da30bb9bf58
Dan Y <liquidcore7@gmail.com>
2019-01-15 15:57:28 +0200

    Bind dummy controller

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Api
 
+from controllers.dummy import DummyController
 from controllers.session import SessionController
 
 app = Flask(__name__)
@@ -8,6 +9,9 @@ global_api = Api(app)
 
 global_api.add_resource(SessionController, '/session')  # GET
 global_api.add_resource(SessionController, '/session/<session_id: int>')  # POST
+
+global_api.add_resource(DummyController, '/test')  # GET
+global_api.add_resource(DummyController, '/test/<data: str>')  # POST
 
 if __name__ == '__main__':
     app.run()
