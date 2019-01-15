247f0397cf9c5b50bc09630d3095a5b045640fe5
Danny <liquidcore7@gmail.com>
2019-01-15 17:11:45 +0200

    remove dummy stuff

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_restful import Api
 
-from controllers.dummy import DummyController
 from controllers.session import SessionController
 
 app = Flask(__name__)
@@ -9,9 +8,6 @@ global_api = Api(app)
 
 global_api.add_resource(SessionController, '/session', endpoint='/session')  # GET
 global_api.add_resource(SessionController, '/session/<session_id>', endpoint='/session/<session_id>')  # POST
-
-global_api.add_resource(DummyController, '/test', endpoint='/test')  # GET
-global_api.add_resource(DummyController, '/test/<data>', endpoint='/test/<data>')  # POST
 
 if __name__ == '__main__':
     app.run()
