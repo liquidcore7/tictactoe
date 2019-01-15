77e914b44b8310d2672c9a7140d27a79475e1e77
Dan Y <liquidcore7@gmail.com>
2019-01-15 17:53:31 +0200

    add wakeup heroku hook

--- controllers/session.py
+++ controllers/session.py
@@ -1,37 +1,52 @@
-from flask_restful import Resource, reqparse
+from flask_restful import Resource
 from typing import *
 from models.state import TicTacToeModel, TicTacToeDelta
+from flask import request
+from models.state import TicTacToeHelper as players
 
 
-class SessionController(Resource):
+class SessionState:
     def __init__(self, max_session: int=1500):
         self.activeSessions: Dict[int, TicTacToeModel.TicTacToeDTO] = dict()
         self.availableIds: Set[int] = set(range(max_session))
-        self.stateParser = reqparse.RequestParser()\
-            .add_argument('player', type=str)\
-            .add_argument('updated_field', type=int)
 
-    def __new_session__(self) -> Optional[int]:
+    def new_session(self) -> Optional[int]:
         if len(self.availableIds) > 0:
             sess_id: int = self.availableIds.pop()
             self.activeSessions[sess_id] = TicTacToeModel.empty()
             return sess_id
         return None
 
-    def __end_session__(self, sess_id: int) -> None:
+    def end_session(self, sess_id: int) -> None:
         del self.activeSessions[sess_id]
         self.availableIds.add(sess_id)
 
+    def get_session(self, sess_id: int) -> TicTacToeModel.TicTacToeDTO:
+        return self.activeSessions[sess_id]
+
+    def update_session(self, sess_id: int, new_state: TicTacToeModel.TicTacToeDTO):
+        if new_state.winner != players.Players.FREE:
+            self.end_session(sess_id)
+        else:
+            self.activeSessions[sess_id] = new_state
+
+
+GlobalSessionState = SessionState()
+
+
+class SessionController(Resource):
+
     def get(self) -> Tuple[Union[str, int], int]:
-        maybe_id: Optional[int] = self.__new_session__()
-        if not maybe_id:
+        maybe_id: Optional[int] = GlobalSessionState.new_session()
+        if maybe_id is None:
             return 'Failed to create session', 500
         return maybe_id, 200
 
     def post(self, session_id: int):
         # TODO: add error handling
-        previous_state: TicTacToeModel.TicTacToeDTO = self.activeSessions[session_id]
-        delta = TicTacToeDelta.from_dict(self.stateParser.parse_args())
+        session_id = int(session_id)
+        previous_state: TicTacToeModel.TicTacToeDTO = GlobalSessionState.get_session(session_id)
+        delta = TicTacToeDelta.from_dict(request.get_json(force=True))
         next_state: TicTacToeModel.TicTacToeDTO = TicTacToeDelta.apply_delta(to=previous_state, delta=delta)
-        self.activeSessions[session_id] = next_state
+        GlobalSessionState.update_session(session_id, next_state)
         return next_state.json(), 200
