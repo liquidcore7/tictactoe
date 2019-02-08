ba747097d340120b510880e6fe3dcbc2f768f22b
Dan Y <liquidcore7@gmail.com>
2019-02-08 20:57:44 +0200

    add tie checker

--- services/bot.py
+++ services/bot.py
@@ -52,23 +52,22 @@ class BotService(WithDataBase[List[TicTacToeDelta.TicTacToeDeltaDTO]]):
         # do player's turn if it's player isn't Players.FREE
         if with_value.player != TicTacToeHelper.Players.FREE:
             maybe_player_field = self.session_service.update_session(session_id, with_value)
-            # update moves for bot
             self.__append__(session_id, with_value)
+
+            if TicTacToeModel.is_won(maybe_player_field):
+                self.delete_session(session_id)
+                return maybe_player_field
 
         # get potential next move if any
         all_moves: List[TicTacToeDelta.TicTacToeDeltaDTO] = self.db.get(session_id + self.BOT_POSTFIX)
-        maybe_next_move: Optional[TicTacToeDelta.TicTacToeDeltaDTO] = self.__next_move__(all_moves)
+        next_move: TicTacToeDelta.TicTacToeDeltaDTO = self.__next_move__(all_moves)
 
-        if maybe_next_move is None:  # if end of game reached, return without bot's move
+        self.__append__(session_id, next_move)
+        resulting_field: TicTacToeModel.TicTacToeDTO = self.session_service.update_session(session_id, next_move)
+
+        if TicTacToeModel.is_won(resulting_field):
             self.delete_session(session_id)
-            return maybe_player_field
-
-        self.__append__(session_id, maybe_next_move)
-        maybe_resulting_field = self.session_service.update_session(session_id, maybe_next_move)
-
-        if maybe_resulting_field.winner != TicTacToeHelper.Players.FREE:
-            self.delete_session(session_id)
-        return maybe_resulting_field
+        return resulting_field
 
     def delete_session(self, session_id: str) -> bool:
         status = self.session_service.delete_session(session_id)
